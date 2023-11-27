05e2f1af567eaf2dc3e83e8771d83d041fd9805b
lawrencenoman-01 <PhinconTraineeOlen@01>
2023-11-27 16:30:28 +0700

    Adding Features for GUIZ: 1. Register & Login (backend) 2. Create jwt and bcrypt

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const { handleResponse } = require('./helper/responseHandler');
 const app = express();
+const routes = require('./routes/index')
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', routes)
 
 app.all('*', (req, res) => {
   return handleResponse(res, 404, { message: 'API Not Found' });
--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,6 @@
 const {
   Model
 } = require('sequelize');
-const bcrypt = require('bcryptjs')
-const salt = bcrypt.genSaltSync(10)
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
