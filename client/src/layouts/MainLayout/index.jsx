76cbfa62ef2b24f62f2dff2ee2b506a8e529e4c6
VLDR <melvernardell@gmail.com>
2023-11-27 13:43:31 +0700

    remove dark mode

--- client/src/layouts/MainLayout/index.jsx
+++ client/src/layouts/MainLayout/index.jsx
@@ -3,26 +3,24 @@ import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectLocale, selectTheme } from '@containers/App/selectors';
+import { selectLocale } from '@containers/App/selectors';
 
 import Navbar from '@components/Navbar';
 
-const MainLayout = ({ children, locale, theme, intl: { formatMessage } }) => (
+const MainLayout = ({ children, locale, intl: { formatMessage } }) => (
   <div>
-    <Navbar title={formatMessage({ id: 'app_title_header' })} locale={locale} theme={theme} />
+    <Navbar title={formatMessage({ id: 'app_title_header' })} locale={locale} />
     {children}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
   locale: selectLocale,
-  theme: selectTheme,
 });
 
 MainLayout.propTypes = {
   children: PropTypes.element.isRequired,
   locale: PropTypes.string,
-  theme: PropTypes.string,
   intl: PropTypes.object,
 };
 
